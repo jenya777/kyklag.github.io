de3138609c97def4e6d92ab5c1bef5f32a952c24
jenya777 <35199068+jenya777@users.noreply.github.com>
2018-01-07 21:21:04 +0300

    grg

---
+++ kukla-lol.com/files/lol_doll_3/js/script.js
@@ -0,0 +1,28 @@
+$(function(){
+    $('a[href^="#"]').click(function (){
+        var elementClick = $(this).attr("href");
+        var destination = $(elementClick).offset().top - 60;
+        jQuery("html:not(:animated), body:not(:animated)").animate({scrollTop: destination}, 800);
+        return false;
+    });
+
+    $('.inside-slider').slick({
+      dots: true,
+    });
+
+});
+ 
+$(document).ready(function() {
+  $('[name="country"]').on('change', function() {
+      var geoKey = $(this).find('option:selected').val();
+      var data = $jsonData.prices[geoKey];
+      var price = data.price;
+      var oldPrice = data.old_price;
+      var currency = data.currency
+      $("[value = "+geoKey+"]").attr("selected", true).siblings().attr('selected', false);
+
+      $('.price_land_s1').text(price);
+      $('.price_land_s2').text(oldPrice);
+      $('.price_land_curr').text(currency);
+  });
+});
